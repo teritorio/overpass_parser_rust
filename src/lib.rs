355b7be87a02958f046084fea5e16f0f53cf2de1
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-07-05 11:08:34 +0200

    Remove wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,2 @@
-use sql_dialect::{postgres::postgres::Postgres, sql_dialect::SqlDialect};
-use wasm_bindgen::prelude::*;
 pub mod overpass_parser;
-use overpass_parser::{parse_query, request::Request};
 pub mod sql_dialect;
-
-#[wasm_bindgen]
-pub fn parse_query_json(query: &str) -> String {
-    match parse_query(query) {
-        Ok(request) => Request::to_sql(
-            &request,
-            &(Box::new(Postgres::default()) as Box<dyn SqlDialect + Send + Sync>),
-            "4326",
-            None,
-        ),
-        Err(e) => format!("Error parsing query: {}", e),
-    }
-}
